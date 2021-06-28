010d524caa6b64cc7b4a91b0debec6d9ac943fb9
Tysek64 <74345334+Tysek64@users.noreply.github.com>
2021-06-28 23:01:37 +0200

    Add files via upload

---
+++ goToTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goToTutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*if (this.gameObject.GetComponent<Collision>().starCounter == 0) {
+            Debug.Log("a");
+            transform.position += new Vector3(46f, 23f, -38f);
+        } else {
+            transform.position = new Vector3(10f, 2f, -11f);
+        }*/
+    }
+    void Update () {
+        //transform.position += new Vector3(46f, 23f, -38f);
+        //Debug.Log(transform.position);
+    }
+}
